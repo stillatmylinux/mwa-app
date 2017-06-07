8c224bd7886cb491cf36b6b09c2bca46172ccf38
Matt Thiessen <matt@thiessen.us>
2017-06-06 21:42:08 -0500

    add canadian states

--- www/js/mwa.js
+++ www/js/mwa.js
@@ -1,6 +1,6 @@
 var mwa = ( typeof mwa == 'undefined' ) ? {} : mwa;
 
-mwa.states = ['ONLINE','AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
+mwa.states = ['ONLINE','AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY','AB','BC','MB','NB','NL','NS','ON','PE','QC','SK'];
 mwa.getStateAbbr = function( id ) {
 	return mwa.states[id];
 }
