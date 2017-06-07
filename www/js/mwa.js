028eee59af0a6d9add98da5ee54c5765ffa27836
Matt Thiessen <matt@thiessen.us>
2017-06-06 21:26:39 -0500

    state index off by 1

--- www/js/mwa.js
+++ www/js/mwa.js
@@ -1,6 +1,6 @@
 var mwa = ( typeof mwa == 'undefined' ) ? {} : mwa;
 
-mwa.states = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
+mwa.states = ['ONLINE','AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
 mwa.getStateAbbr = function( id ) {
 	return mwa.states[id];
 }
